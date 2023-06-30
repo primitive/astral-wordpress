db9542c0ab567b5a2723f5abfee01d8c6550e878
Shaun Knowles <shaun@primitivedigital.uk>
2023-06-30 13:08:56 +0100

    test ai code gen

---
+++ src/components/WordPressPosts.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const WordPressContent = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        // sk-dev todo: extract config to a separate file
+        // const response = await axios.get('https://your-wordpress-site/wp-json/wp/v2/posts');
+        const response = await axios.get('https://sknow.it/wp-json/wp/v2/posts');
+
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching WordPress posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h2>WordPress Posts</h2>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title.rendered}</h3>
+          <p>{post.content.rendered}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default WordPressContent;
