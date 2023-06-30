bb2dc119486628ee2c71243710905daa4199cf69
Shaun Knowles <shaun@primitivedigital.uk>
2023-06-30 17:08:06 +0100

    ass posts page

---
+++ src/pages/Posts.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import WPPosts from '../components/WPPosts';
+import './Posts.css';
+
+const Posts: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Posts</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Posts</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <WPPosts />
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Posts;
