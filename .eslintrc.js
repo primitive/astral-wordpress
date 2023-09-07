4904d9e8d53dcb2f8f06c68749a8f497175b5dfa
Shaun Knowles <shaun@primitivedigital.uk>
2023-09-07 09:53:02 +0100

    update: vite + config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
-    es2021: true
+    node: true
   },
-  extends: [
-    'eslint:recommended',
+  'extends': [
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'eslint:recommended'
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  settings: {
-    react: {
-      version: 'detect'
-    }
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'astral-wordpress',
-  webDir: 'build',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
